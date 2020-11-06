27b1ca23e6454a4689ce8916cb0a28ecfc874c37
lucasvalcant <73969258+lucasvalcant@users.noreply.github.com>
2020-11-06 18:45:42 -0300

    add csv e dot

---
+++ arvore.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import tree
+
+train = pd.read_csv('interin_data/database_discrete.csv')
+y_train = train['caro']
+x_train = train.drop(['caro'], axis=1).values
+decision_tree = tree.DecisionTreeClassifier(max_depth = 20)
+decision_tree.fit(x_train, y_train)
+
+with open("discrete.dot", 'w') as f:
+    f = tree.export_graphviz(decision_tree,
+                             out_file=f,
+                             max_depth=20,
+                             impurity= True,
+                             feature_names = list(train.drop(['caro'], axis=1)),
+                             class_names = ['False', 'True'],
+                             rounded = True,
+                             filled = True)
--- init.py
+++ init.py
@@ -4,13 +4,16 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
+'''
+Define onde buscar o database
+'''
 path = os.path.abspath(__file__)
 path = os.path.dirname(path)
 print(path)
+#Carrega o dataframe
 df = pd.read_excel(path+os.sep+"raw_data"+os.sep+"database.xlsx")
-
-
+#Corta o dataframe só com a coluna caro
+df2 = df[["caro"]]
 def add_kmeans(column,cluster,plot = False):
 
     kmeans = KMeans(n_clusters=cluster, random_state=0)
@@ -23,7 +26,7 @@ def add_kmeans(column,cluster,plot = False):
         cuted_list.append(discrete[0])
 
 
-    df.insert(2, "discretization_"+column, cuted_list, True)
+    df2.insert(0, "discretization_"+column, cuted_list, True)
     x = range(0, len(df[["price"]]))
     if plot:
         sns.scatterplot(x=x, y=column, data=df, hue="discretization_"+column)
@@ -42,7 +45,7 @@ def add_2kmeans(column1,column2,cluster):
         cuted_list.append(discrete[0])
 
 
-    df.insert(2, "discretization_"+column1+"_"+column2, cuted_list, True)
+    df.insert(0, "discretization_"+column1+"_"+column2, cuted_list, True)
 
 
 add_2kmeans("lat", "long", 2)
@@ -57,9 +60,12 @@ add_kmeans("grade", 4, plot=False)
 add_kmeans("sqft_above", 3, plot=False)
 add_kmeans("sqft_basement", 3, plot=False)
 add_kmeans("yr_built", 3, plot=False)
-add_kmeans("yr_renovated", 2, plot=True)
-add_kmeans("sqft_living15", 3, plot=True)
-add_kmeans("sqft_lot15", 3, plot=True)
-df.to_excel(path+os.sep+"interin_data"+os.sep+"database_discrete.xlsx")
+add_kmeans("yr_renovated", 2, plot=False)
+add_kmeans("sqft_living15", 3, plot=False)
+add_kmeans("sqft_lot15", 3, plot=False)
+
+
+df2.to_excel(path+os.sep+"interin_data"+os.sep+"database_discrete.xlsx")
+df2.to_csv(path+os.sep+"interin_data"+os.sep+"database_discrete.csv")
 
 print("oi")
