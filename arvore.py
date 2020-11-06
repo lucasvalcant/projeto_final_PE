96e22d61ae4c42d887ccd63cf11da4a56bac218e
lucasvalcant <73969258+lucasvalcant@users.noreply.github.com>
2020-11-06 19:38:36 -0300

    discrete_20.dot adicionado

--- arvore.py
+++ arvore.py
@@ -9,13 +9,13 @@ from sklearn import tree
 train = pd.read_csv('interin_data/database_discrete.csv')
 y_train = train['caro']
 x_train = train.drop(['caro'], axis=1).values
-decision_tree = tree.DecisionTreeClassifier(max_depth = 10)
+decision_tree = tree.DecisionTreeClassifier(max_depth = 20)
 decision_tree.fit(x_train, y_train)
 
-with open("discrete_10.dot", 'w') as f:
+with open("discrete_20.dot", 'w') as f:
     f = tree.export_graphviz(decision_tree,
                              out_file=f,
-                             max_depth=10,
+                             max_depth=20,
                              impurity= True,
                              feature_names = list(train.drop(['caro'], axis=1)),
                              class_names = ['False', 'True'],
