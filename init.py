34ea4410f05bd8e3bce73357497632713db91813
lucasvalcant <73969258+lucasvalcant@users.noreply.github.com>
2020-11-06 00:25:34 -0300

    função que utiliza Kmeans foi criada e discretizou todas as colunas

--- init.py
+++ init.py
@@ -1,34 +1,63 @@
 import pandas as pd
 import os
+import numpy as np
 import seaborn as sns
-
 import matplotlib.pyplot as plt
-
-
-
 from sklearn.cluster import KMeans
 
 path = os.path.abspath(__file__)
 path = os.path.dirname(path)
 print(path)
 df = pd.read_excel(path+os.sep+"raw_data"+os.sep+"database.xlsx")
-kmeans = KMeans(n_clusters=2, random_state=0)
-df_cut = df[["lat", "long"]].values
-kmeans.fit(df_cut)
-disc_list = []
+def add_kmeans(column,cluster,plot = False):
 
-for line in df_cut:
+    kmeans = KMeans(n_clusters=cluster, random_state=0)
+    df_cuted = df[[column]].values
+    kmeans.fit(df_cuted)
+    cuted_list = []
 
-    disc = kmeans.predict([line])
-    disc_list.append(disc[0])
-    print(disc)
+    for line in df_cuted:
+        discrete = kmeans.predict([line])
+        cuted_list.append(discrete[0])
 
-df.insert(2, "discretization_lat_and_long", disc_list, True)
 
-sns.scatterplot(x = "lat", y = "long", data = df, hue = "discretization_lat_and_long")
+    df.insert(2, "discretization_"+column, cuted_list, True)
+    x = range(0, len(df[["price"]]))
+    if plot:
+        sns.scatterplot(x=x, y=column, data=df, hue="discretization_"+column)
+        plt.show()
 
-plt.show()
 
-df.to_excel(path+os.sep+"interin_data"+os.sep+"database_latlong.xlsx")
+def add_2kmeans(column1,column2,cluster):
+
+    kmeans = KMeans(n_clusters=cluster, random_state=0)
+    df_cuted = df[[column1,column2]].values
+    kmeans.fit(df_cuted)
+    cuted_list = []
+
+    for line in df_cuted:
+        discrete = kmeans.predict([line])
+        cuted_list.append(discrete[0])
+
+
+    df.insert(2, "discretization_"+column1+"_"+column2, cuted_list, True)
+
+
+add_2kmeans("lat", "long", 2)
+add_kmeans("sqft_living", 2, plot=False)
+add_kmeans("sqft_lot", 3, plot=False)
+add_kmeans("bathrooms", 4, plot=False)
+add_kmeans("bedrooms", 3, plot=False)
+add_kmeans("floors", 3, plot=False)
+add_kmeans("view", 2, plot=False)
+add_kmeans("condition", 3, plot=False)
+add_kmeans("grade", 4, plot=False)
+add_kmeans("sqft_above", 3, plot=False)
+add_kmeans("sqft_basement", 3, plot=False)
+add_kmeans("yr_built", 3, plot=False)
+add_kmeans("yr_renovated", 2, plot=True)
+add_kmeans("sqft_living15", 3, plot=True)
+add_kmeans("sqft_lot15", 3, plot=True)
+#df.to_excel(path+os.sep+"interin_data"+os.sep+"database_latlong.xlsx")
 
 print("oi")
